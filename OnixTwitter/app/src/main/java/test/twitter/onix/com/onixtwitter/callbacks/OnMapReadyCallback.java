c59c8f420f1132ba535ca4fe945635c8dc143bfc
3888 <krrg@mail.ru>
2015-10-12 08:54:00 +0300

    some fix

--- OnixTwitter/app/src/main/java/test/twitter/onix/com/onixtwitter/callbacks/OnMapReadyCallback.java
+++
@@ -1,7 +0,0 @@
-package test.twitter.onix.com.onixtwitter.callbacks;
-
-import com.google.android.gms.maps.GoogleMap;
-
-public interface OnMapReadyCallback {
-    void onMapReady (GoogleMap var1);
-}
